28fd7c05bf2d97442a698d197679a846051b2be8
Ulises <uliseslf99@hotmail.com>
2019-04-28 13:57:20 -0500

    Connection set

--- src/main/java/com/rts/example/maven/src/Game.java
+++ src/main/java/com/rts/example/maven/src/Game.java
@@ -5,14 +5,31 @@
  */
 package com.rts.example.maven.src;
 
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ulise
  */
 public class Game {
     public static void main(String args[]){
-        System.out.println("Running");
-        String dbUrl = System.getenv("DATABASE_URL");
-        System.out.println(dbUrl);
+        try {
+            getConnection();
+            System.out.println("Connected ");
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+    
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+    String dbUrl = System.getenv("JDBC_DATABASE_URL");
+    return DriverManager.getConnection(dbUrl);
+}
 }
